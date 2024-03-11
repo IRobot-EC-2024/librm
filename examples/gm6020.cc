72c12504292c6b35d3d0516a80ea7ce8a2065306
lunarifish <messenger@lunariio.work>
2024-03-11 21:33:31 +0800

    refactor: 修改gm6020例程

--- examples/gm6020.cc
+++ examples/gm6020.cc
@@ -12,26 +12,18 @@ using components::motor::GM6020;
 using modules::algorithm::PID::PIDType;
 using modules::algorithm::PID::RingPID;
 
-#ifdef __cplusplus
 extern "C" {
-#endif
 
 [[noreturn]] void ExampleGM6020Task(const void *pv_arg) {
   UNUSED(pv_arg);
 
-  auto *gm6020 = new GM6020(&hcan1, 1);
-  auto *ringPID = new RingPID(PIDType::kPosition, 5, 0, 0, 30000, 0, 8191);
+  GM6020 gm6020(&hcan1, 1);
+  RingPID ringPID(PIDType::kPosition, 5, 0, 0, 30000, 0, 8191);
 
   for (;;) {
     osDelay(1);
-    ringPID->update(3000, gm6020->encoder());
-    gm6020->SetCurrent(static_cast<int16_t>(ringPID->value()));
+    ringPID.update(3000, gm6020.encoder());
+    gm6020.SetCurrent(static_cast<int16_t>(ringPID.value()));
   }
-
-  delete gm6020;
-  delete ringPID;
 }
-
-#ifdef __cplusplus
 }
-#endif
