a047b72fc7a4e0939fd8d17b8f860b7883172450
lunarifish <messenger@lunariio.work>
2024-03-19 22:02:27 +0800

    feat: 计划增加大疆电机组类，应对一条报文控制4个电机的需求

---
+++ framework/components/motor/dji/dji_motor_group.hpp
@@ -0,0 +1,31 @@
+
+// TODO: 未完成
+
+/**
+ * @file  components/motor/dji/dji_motor_group.hpp
+ * @brief 大疆电机组
+ * @note  得益于大疆电机的通信协议，一条CAN报文可以控制最多4个电机。
+ *        本文件中的"电机组"类封装了这种特性，可以减少CAN总线上的消息数量。
+ *        若有控制多个电机的需求，建议使用本文件中的类，而不是实例化多个电机对象分别控制。
+ */
+
+#ifndef EC_LIB_COMPONENTS_MOTOR_DJI_MOTOR_GROUP_HPP
+#define EC_LIB_COMPONENTS_MOTOR_DJI_MOTOR_GROUP_HPP
+
+#include <vector>
+
+#include "dji_motor.h"
+#include "modules/typedefs.h"
+
+namespace components::motor {
+
+class DjiMotorGroup final {
+ public:
+
+ private:
+  ::std::vector<DjiMotorBase> motors_;
+};
+
+}
+
+#endif  // EC_LIB_COMPONENTS_MOTOR_DJI_MOTOR_GROUP_HPP
