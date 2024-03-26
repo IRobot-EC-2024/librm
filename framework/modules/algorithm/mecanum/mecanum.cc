eb02ac56fcae4e7eba143c770be1576992706cad
lunarifish <messenger@lunariio.work>
2024-03-27 01:41:46 +0800

    feat: 麦轮解算

---
+++ framework/modules/algorithm/mecanum/mecanum.cc
@@ -0,0 +1,47 @@
+/**
+ * @file  modules/algorithm/mecanum/mecanum.cpp
+ * @brief 麦轮正运动学结算
+ */
+
+#include "mecanum.h"
+
+using namespace irobot_ec::modules::algorithm;
+
+/**
+ * @param wheel_base    轮子间距
+ * @param wheel_track   轮子轴距
+ */
+Mecanum::Mecanum(fp32 wheel_base, fp32 wheel_track) : wheel_base_(wheel_base), wheel_track_(wheel_track), speeds_{0} {}
+
+/**
+ * @brief 计算麦轮底盘的四个轮子的速度
+ * @param vx    x轴方向的速度
+ * @param vy    y轴方向的速度
+ * @param wz    z轴方向的角速度
+ */
+void Mecanum::Calculate(fp32 vx, fp32 vy, fp32 wz) {
+  this->speeds_[0] = vx - vy - (this->wheel_base_ + this->wheel_track_) * wz;
+  this->speeds_[1] = vx + vy + (this->wheel_base_ + this->wheel_track_) * wz;
+  this->speeds_[2] = vx + vy - (this->wheel_base_ + this->wheel_track_) * wz;
+  this->speeds_[3] = vx - vy + (this->wheel_base_ + this->wheel_track_) * wz;
+}
+
+/**
+ * @return  左前轮速度
+ */
+[[nodiscard]] fp32 Mecanum::v_lf() const { return this->speeds_[0]; }
+
+/**
+ * @return  右前轮速度
+ */
+[[nodiscard]] fp32 Mecanum::v_rf() const { return this->speeds_[1]; }
+
+/**
+ * @return  左后轮速度
+ */
+[[nodiscard]] fp32 Mecanum::v_lb() const { return this->speeds_[2]; }
+
+/**
+ * @return  右后轮速度
+ */
+[[nodiscard]] fp32 Mecanum::v_rb() const { return this->speeds_[3]; }
---
+++ framework/modules/algorithm/mecanum/mecanum.h
@@ -0,0 +1,42 @@
+/**
+ * @file  modules/algorithm/mecanum/mecanum.h
+ * @brief 麦轮正运动学结算
+ */
+
+#ifndef EC_LIB_MODULES_ALGORITHM_MECANUM_MECANUM_H
+#define EC_LIB_MODULES_ALGORITHM_MECANUM_MECANUM_H
+
+#include <array>
+
+#include "modules/typedefs.h"
+
+namespace irobot_ec::modules::algorithm {
+
+/**
+ * @brief 麦轮正运动学结算
+ */
+class Mecanum {
+ public:
+  Mecanum() = delete;
+  ~Mecanum() = default;
+  Mecanum(fp32 wheel_base, fp32 wheel_track);
+
+  // no copy
+  Mecanum(const Mecanum &) = delete;
+  Mecanum &operator=(const Mecanum &) = delete;
+
+  void Calculate(fp32 vx, fp32 vy, fp32 wz);
+  [[nodiscard]] fp32 v_lf() const;
+  [[nodiscard]] fp32 v_rf() const;
+  [[nodiscard]] fp32 v_lb() const;
+  [[nodiscard]] fp32 v_rb() const;
+
+ private:
+  fp32 wheel_base_;    // 轮子间距
+  fp32 wheel_track_;   // 轮子轴距
+  fp32 speeds_[4];
+};
+
+}  // namespace irobot_ec::modules::algorithm
+
+#endif  // EC_LIB_MODULES_ALGORITHM_MECANUM_MECANUM_H
