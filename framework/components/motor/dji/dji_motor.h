31740d4791bf74bb262bd5dece83500598f5304f
lunarifish <messenger@lunariio.work>
2024-03-29 20:32:19 +0800

    fix: 大疆电机现在完全能用了 - +更新例程

--- framework/components/motor/dji/dji_motor.h
+++ framework/components/motor/dji/dji_motor.h
@@ -12,15 +12,13 @@
 #ifdef HAL_CAN_MODULE_ENABLED
 
 #include <array>
+#include <list>
 #include <unordered_map>
 
 #include "hal_wrapper/hal_can.h"
 #include "modules/typedefs.h"
 
 namespace irobot_ec::components::motor {
-
-template <typename MotorType>
-class DjiMotorGroup;
 
 /**
  * @brief     DJI电机的基类
@@ -29,11 +27,9 @@ class DjiMotorGroup;
  */
 
 class DjiMotorBase : public irobot_ec::hal::can::CanDeviceBase {
-  template <typename MotorType>
-  friend class DjiMotorGroup;
-
  public:
   DjiMotorBase() = delete;
+  static void SendCommand();
 
   /* 取值函数 */
   [[nodiscard]] uint16_t encoder() const;
@@ -46,8 +42,7 @@ class DjiMotorBase : public irobot_ec::hal::can::CanDeviceBase {
   DjiMotorBase(uint8_t buffer_index, CAN_HandleTypeDef *hcan, uint16_t id);
 
   template <int16_t current_bound, uint8_t buffer_index>
-  void SetCurrentTemplate(int16_t current, bool push_message);
-
+  void SetCurrentTemplate(int16_t current);
   void RxCallback(irobot_ec::hal::can::CanRxMsg *msg) override;
 
   uint16_t id_{};  // 电机ID
@@ -59,10 +54,15 @@ class DjiMotorBase : public irobot_ec::hal::can::CanDeviceBase {
   /***********************/
 
   /**
-   * @note 一条CAN总线一个缓冲区，这是三个电机类的缓冲区查找表。
-   * @note 0 :GM6020, 1: M3508, 2: M2006
+   * @note 三个型号的电机各自的发送缓冲区
+   * @note 每种电机对应一个缓冲区组，每个缓冲区组是一个键值对字典(can总线指针, 18字节的缓冲区数组)
+   * @note 缓冲区的前16字节是电机控制数据，最后两字节是发送标志(buf[17]: 前八字节是否发送, buf[16]: 后八字节是否发送)
    */
-  static std::array<std::unordered_map<CAN_HandleTypeDef *, std::array<uint8_t, 16>> *, 3> tx_bufs_;
+  static std::array<std::unordered_map<CAN_HandleTypeDef *, std::array<uint8_t, 18>> *, 3> tx_bufs_;
+  std::array<uint8_t, 18> *tx_buffer_p_;  // 指向这个电机发送缓冲区的指针
+
+ private:
+  static std::list<DjiMotorBase *> motor_list_;  // 电机对象链表
 };
 
 /**
@@ -74,9 +74,6 @@ class DjiMotorBase : public irobot_ec::hal::can::CanDeviceBase {
  * @note https://www.robomaster.com/zh-CN/products/components/general/GM6020
  */
 class GM6020 final : public DjiMotorBase {
-  template <typename MotorType>
-  friend class DjiMotorGroup;
-
  public:
   GM6020() = delete;
   GM6020(CAN_HandleTypeDef *hcan, uint16_t id);
@@ -85,7 +82,7 @@ class GM6020 final : public DjiMotorBase {
   GM6020(const GM6020 &) = delete;
   GM6020 &operator=(const GM6020 &) = delete;
 
-  void SetCurrent(int16_t current, bool push_message = true);
+  void SetCurrent(int16_t current);
 };
 
 /**
@@ -97,9 +94,6 @@ class GM6020 final : public DjiMotorBase {
  * @note https://www.robomaster.com/zh-CN/products/components/general/M2006
  */
 class M2006 final : public DjiMotorBase {
-  template <typename MotorType>
-  friend class DjiMotorGroup;
-
  public:
   M2006() = delete;
   M2006(CAN_HandleTypeDef *hcan, uint16_t id);
@@ -108,7 +102,7 @@ class M2006 final : public DjiMotorBase {
   M2006(const M2006 &) = delete;
   M2006 &operator=(const M2006 &) = delete;
 
-  void SetCurrent(int16_t current, bool push_message = true);
+  void SetCurrent(int16_t current);
 };
 
 /**
@@ -120,9 +114,6 @@ class M2006 final : public DjiMotorBase {
  * @note https://www.robomaster.com/zh-CN/products/components/general/M3508
  */
 class M3508 final : public DjiMotorBase {
-  template <typename MotorType>
-  friend class DjiMotorGroup;
-
  public:
   M3508() = delete;
   M3508(CAN_HandleTypeDef *hcan, uint16_t id);
@@ -131,7 +122,7 @@ class M3508 final : public DjiMotorBase {
   M3508(const M3508 &) = delete;
   M3508 &operator=(const M3508 &) = delete;
 
-  void SetCurrent(int16_t current, bool push_message = true);
+  void SetCurrent(int16_t current);
 };
 
 }  // namespace irobot_ec::components::motor
