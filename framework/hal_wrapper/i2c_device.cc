1840049b6fffd6de8ce42cf97247272bb7e4b81b
lunarifish <messenger@lunariio.work>
2024-03-30 18:10:29 +0800

    feat: ist8310驱动移植完成

---
+++ framework/hal_wrapper/i2c_device.cc
@@ -0,0 +1,41 @@
+
+#include "i2c_device.h"
+
+#if defined(HAL_I2C_MODULE_ENABLED)
+
+namespace irobot_ec::hal {
+
+/**
+ * @param hi2c I2C外设句柄
+ * @param addr I2C设备地址
+ */
+I2cDevice::I2cDevice(I2C_HandleTypeDef &hi2c, uint8_t addr) : hi2c_(&hi2c), addr_(addr) {}
+
+/**
+ * @brief 写数据
+ * @param reg   寄存器地址
+ * @param data  待写入的数据
+ * @param size  数据长度
+ */
+void I2cDevice::Write(uint8_t reg, uint8_t *data, uint16_t size) {
+  HAL_I2C_Mem_Write(this->hi2c_, this->addr_ << 1, reg, I2C_MEMADD_SIZE_8BIT, data, size, 10);
+}
+
+/**
+ * @brief 读数据
+ * @param reg   寄存器地址
+ * @param size  数据长度
+ */
+void I2cDevice::Read(uint8_t reg, uint16_t size) {
+  HAL_I2C_Mem_Read(this->hi2c_, this->addr_ << 1, reg, I2C_MEMADD_SIZE_8BIT, this->buffer_, size, 1000);
+}
+
+/**
+ * @brief 获取缓冲区指针
+ * @return 缓冲区指针
+ */
+const uint8_t *I2cDevice::buffer() const { return this->buffer_; }
+
+}  // namespace irobot_ec::hal
+
+#endif
---
+++ framework/hal_wrapper/i2c_device.h
@@ -0,0 +1,34 @@
+
+#ifndef EC_LIB_HAL_WRAPPER_I2C_DEVICE_H
+#define EC_LIB_HAL_WRAPPER_I2C_DEVICE_H
+
+#include "hal.h"
+#if defined(HAL_I2C_MODULE_ENABLED)
+
+namespace irobot_ec::hal {
+
+/**
+ * @brief I2C设备
+ */
+class I2cDevice {
+ public:
+  I2cDevice(I2C_HandleTypeDef &hi2c, uint8_t addr);
+  I2cDevice() = delete;
+  ~I2cDevice() = default;
+
+  void Write(uint8_t reg, uint8_t *data, uint16_t size);
+  void Read(uint8_t reg, uint16_t size);
+
+  [[nodiscard]] const uint8_t *buffer() const;
+
+ protected:
+  I2C_HandleTypeDef *hi2c_;
+  uint8_t addr_;
+  uint8_t buffer_[8]{0};
+};
+
+}  // namespace irobot_ec::hal
+
+#endif
+
+#endif  // EC_LIB_HAL_WRAPPER_I2C_DEVICE_H
