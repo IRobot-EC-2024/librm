8e371a0c1c9184db31a2a404243fe8cc61581dfc
lunarifish <messenger@lunariio.work>
2024-03-31 02:34:41 +0800

    style: lint

--- framework/modules/exception/exception.cc
+++ framework/modules/exception/exception.cc
@@ -2,9 +2,9 @@
 #include "exception.h"
 
 namespace irobot_ec::modules::exception {
-  /**
+/**
  * @brief 抛出异常
  * @param e 异常类型
-   */
-  void ThrowException(Exception e) {}
-}
+ */
+void ThrowException(Exception e) {}
+}  // namespace irobot_ec::modules::exception
