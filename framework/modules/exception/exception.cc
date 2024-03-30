69cef5a5cbaffb192c2da005dd1456019eaba115
lunarifish <messenger@lunariio.work>
2024-03-31 02:31:36 +0800

    fix: fix typo

--- framework/modules/exception/exception.cc
+++ framework/modules/exception/exception.cc
@@ -1,10 +1,10 @@
 
 #include "exception.h"
 
-using namespace irobot_ec::modules::exception;
-
-/**
+namespace irobot_ec::modules::exception {
+  /**
  * @brief 抛出异常
  * @param e 异常类型
- */
-void throwException(Exception e) {}
+   */
+  void ThrowException(Exception e) {}
+}
